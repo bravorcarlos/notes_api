598bea082d34e23f4039babe8aeddf3e871e3885
bravorcarlos <bravorcarlos1@gmail.com>
2024-11-04 14:25:29 -0400

    documentation fixed

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from notes.urls import router_notes
@@ -30,7 +31,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   # permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
